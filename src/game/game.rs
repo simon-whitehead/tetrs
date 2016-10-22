5ce35be0935e182be4993845bae287a6e571115d
Simon Whitehead <chemnova@gmail.com>
2016-10-22 11:49:47 +1100

    Slight refactoring

--- src/game/game.rs
+++ src/game/game.rs
@@ -7,6 +7,7 @@ pub struct Game {
     time: f64,
     rec_y: f64,
     next_block_move: f64,
+    next_block_move_time: f64,
 }
 
 impl Game {
@@ -15,21 +16,29 @@ impl Game {
             time: 0.0,
             rec_y: 0.0,
             next_block_move: 1.0,
+            next_block_move_time: 0.5,
         }
     }
 
     pub fn process(&mut self, e: &Event) -> bool {
         match *e {
             Event::Update(update) => {
-                if self.time - self.next_block_move > 0.5 {
-                    self.rec_y = self.rec_y + 50.0;
-                    self.next_block_move = self.time + 1.0;
-                }
                 self.time = self.time + update.dt;
             }
             _ => (),
         }
+
+        self.drop_active_block();
+
         true
+    }
+
+    fn drop_active_block(&mut self) {
+        // Move the active block down if its been longer than the wait time
+        if self.time - self.next_block_move > self.next_block_move_time {
+            self.rec_y = self.rec_y + 50.0;
+            self.next_block_move = self.time + 1.0;
+        }
     }
 
     pub fn render(&self, window: &mut GameWindow, e: &Event) {
