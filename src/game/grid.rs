907f957d38ec8a64976fc5f4f9029efbfc3738c7
Simon Whitehead <chemnova@gmail.com>
2016-10-23 17:25:45 +1100

    Made blocks optional

--- src/game/grid.rs
+++ src/game/grid.rs
@@ -8,12 +8,12 @@ const GRID_OFFSET: f64 = 20.0;
 const TILE_SIZE: f64 = 29.0;
 
 pub struct Grid {
-    boxes: [[Block; 10]; 22],
+    boxes: [[Option<Block>; 10]; 22],
 }
 
 impl Grid {
     pub fn new() -> Grid {
-        Grid { boxes: [[Block::new([0.0, 0.0, 0.0, 1.0]); 10]; 22] }
+        Grid { boxes: [[None; 10]; 22] }
     }
 
     pub fn render<G>(&self, context: Context, gfx: &mut G, e: &Event)
@@ -22,7 +22,11 @@ impl Grid {
         for mut y in 2..22 {
             for mut x in 0..10 {
                 let adjusted_y = y - 2;
-                rectangle(self.boxes[y][x].color,
+                let color = match self.boxes[y][x] {
+                    Some(ref block) => block.color,
+                    None => [0.0, 0.0, 0.0, 1.0],
+                };
+                rectangle(color,
                           [x as f64 * TILE_SIZE + 10.0,
                            adjusted_y as f64 * TILE_SIZE + 10.0,
                            TILE_SIZE as f64,
