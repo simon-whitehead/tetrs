d73cf10041b9eb7f9e6e468022fa1f8bb379d220
Simon Whitehead <chemnova@gmail.com>
2016-10-29 22:29:37 +1100

    Stopped the Rectangle allocation per render

--- src/game/block.rs
+++ src/game/block.rs
@@ -5,11 +5,22 @@ use game::config::Config;
 #[derive(Copy, Clone)]
 pub struct Block {
     pub color: [f32; 4],
+    rect: rectangle::Rectangle,
 }
 
 impl Block {
     pub fn new(color: [f32; 4]) -> Block {
-        Block { color: color }
+        let rect = rectangle::Rectangle::new(color)
+            .shape(rectangle::Shape::Bevel(1.0))
+            .border(rectangle::Border {
+                color: [0.0, 0.0, 0.0, 1.0],
+                radius: 1.0,
+            });
+
+        Block {
+            color: color,
+            rect: rect,
+        }
     }
 
     pub fn render<G>(&self,
@@ -25,21 +36,12 @@ impl Block {
         let tile_size = config.tile_size;
         let adjusted_y = y - 2;
 
-        let rect = rectangle::Rectangle {
-            color: self.color,
-            shape: rectangle::Shape::Bevel(1.0),
-            border: Some(rectangle::Border {
-                color: [0.0, 0.0, 0.0, 1.0],
-                radius: 1.0,
-            }),
-        };
-
-        rect.draw([x as f64 * tile_size + grid_offset,
-                   adjusted_y as f64 * tile_size + grid_offset,
-                   tile_size as f64,
-                   tile_size as f64],
-                  &Default::default(),
-                  context.transform,
-                  gfx);
+        self.rect.draw([x as f64 * tile_size + grid_offset,
+                        adjusted_y as f64 * tile_size + grid_offset,
+                        tile_size as f64,
+                        tile_size as f64],
+                       &Default::default(),
+                       context.transform,
+                       gfx);
     }
 }
